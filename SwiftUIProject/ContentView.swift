adcc4d0a4fe221c1e38a9d00fff12a66164140ac
Igor Polousov <igorpolousov@gmail.com>
2022-04-25 09:36:48 +0300

    Initial Commit

---
+++ SwiftUIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIProject
+//
+//  Created by Igor Polousov on 25.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Igor Polousov on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
