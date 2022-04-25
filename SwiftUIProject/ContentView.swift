93da745c2dc198a21fc555edad9ff463ae112db7
Igor Polousov <igorpolousov@gmail.com>
2022-04-25 14:03:55 +0300

    + keyboard notifications + ZStack swift image + scroll view

--- SwiftUIProject/ContentView.swift
+++ SwiftUIProject/ContentView.swift
@@ -6,11 +6,67 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State private var login = ""
+    @State private var password = ""
+    @State private var shouldShowLogo: Bool = true
+    
+    // from Combime properties
+    private let keyboardIsOnPublisher = Publishers.Merge(
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+            .map {_ in true},
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map {_ in false}
+    )
+        .removeDuplicates()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack{
+            GeometryReader { _ in
+                Image("swift-og")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+            }
+            
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    if shouldShowLogo {
+                        Text("VK App SwiftUI")
+                            .font(.largeTitle)
+                            .padding(.top, 32)
+                    }
+                    // Login Password fields
+                    VStack {
+                        HStack{
+                            Text("Login:")
+                            Spacer()
+                            TextField("",text: $login)
+                                .frame(maxWidth: 150)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                        }
+                        HStack {
+                            Text("Password:")
+                            Spacer()
+                            SecureField("", text: $password)
+                                .frame(maxWidth: 150)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                        }
+                    }
+                }.frame(maxWidth: 250)
+                    .padding(.top, 50)
+                
+                // Login button
+                Button(action: {print("Hello there")}) {
+                    Text("Login")
+                }
+                .padding(.top, 50)
+                .padding(.bottom, 20)
+                .disabled(login.isEmpty || password.isEmpty)
+            }
+        }
     }
 }
 
